b9c0961f050efec22abdd9d1e42ebc478e1fa45c
Ravi Singh Lodhi <ravi@qodeleaf.com>
2021-08-15 10:51:38 +0530

    Added deploying script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,22 @@
 require("@nomiclabs/hardhat-waffle");
 
+const ALCHEMY_API_KEY = "sFOM0wCFLA5nEj2-EiOkMVM4GFM0pDYs";
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const ROPSTEN_PRIVATE_KEY = "d76565a01777631f57827f34235248dff2316f88f1c3e188a8ed307a1d22014e";
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.7",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    const MyToken = await ethers.getContractFactory("MyToken");
+    const token = await MyToken.deploy();
+  
+    console.log("Token address:", token.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
